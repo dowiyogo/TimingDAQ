8435eb0c150a00812014d9e4849af73e50f82844
idutta <irene.dutta@cern.ch>
2024-04-26 11:40:19 -0500

    latest changes

---
+++ setup_cmslpc_python3_1.sh
@@ -0,0 +1,13 @@
+
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_101/Python/3.9.6/x86_64-centos7-clang12-opt/Python-env.sh
+# source /cvmfs/sft.cern.ch/lcg/releases/LCG_100/Python/3.8.6/x86_64-centos7-clang11-opt/Python-env.sh
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_100/ROOT/v6.24.00/x86_64-centos7-clang11-opt/ROOT-env.sh
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_100/scipy/1.5.1/x86_64-centos7-clang11-opt/scipy-env.sh
+
+# source /cvmfs/sft.cern.ch/lcg/releases/LCG_88/gcc/4.9.3/x86_64-slc6/setup.sh
+# source /cvmfs/sft.cern.ch/lcg/releases/LCG_96python3/Python/3.6.5/x86_64-slc6-gcc8-opt/Python-env.sh
+# source /cvmfs/sft.cern.ch/lcg/releases/LCG_88/ROOT/6.08.06/x86_64-slc6-gcc49-opt/bin/thisroot.sh
+
+# source /cvmfs/sft.cern.ch/lcg/releases/LCG_96python3/Python/3.6.5/x86_64-slc6-gcc8-opt/Python-env.sh
+# source /cvmfs/sft.cern.ch/lcg/releases/LCG_88/ROOT/6.08.06/x86_64-slc6-gcc49-opt/ROOT-env.sh
+# source /cvmfs/sft.cern.ch/lcg/releases/LCG_88/scipy/0.18.1/x86_64-slc6-gcc49-opt/scipy-env.sh
